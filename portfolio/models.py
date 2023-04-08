00947b1e8954048f3fc74e6ca3a7eb147c70194f
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-04-07 21:16:28 -0500

    improving the design of the portfolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField((""), max_length=100)
-    description = models.CharField((""), max_length=250)
-    image = models.ImageField((""), upload_to='portfolio/images/')
-    image2 = models.ImageField(upload_to='portfolio/images/', blank=True, null=True)
+    tools_choices=[
+        ('django', 'Django'),
+        ('javascript', 'JavaScript'),
+        ('html', 'HTML'),
+        ('css', 'CSS'),
+        ('python', 'Python'),
+    ]
+    
+    title = models.CharField(("Title"), max_length=100,)
+    description = models.CharField(("Description"), max_length=250)
+    image = models.ImageField(("Image"), upload_to='portfolio/images/')
+    image2 = models.ImageField(("Image - (optional)"),upload_to='portfolio/images/', blank=True, null=True)
     url = models.URLField(blank=True)
+    tools_box=models.CharField(max_length=255, choices=tools_choices, default='django', verbose_name='Used tools')
+    tools_box2=models.CharField(max_length=255,choices=tools_choices, blank=True)
+    tools_box3=models.CharField(max_length=255,choices=tools_choices, blank=True)
+    tools_box4=models.CharField(max_length=255,choices=tools_choices, blank=True)
