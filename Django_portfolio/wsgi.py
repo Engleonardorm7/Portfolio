9e7dc0bd7346c54420da24f6398d242e19c961b6
Leonardo <leonardorm7@hotmail.com>
2023-06-26 19:18:47 -0500

    Django_portfolio.settings added

--- Django_portfolio/wsgi.py
+++ Django_portfolio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Protfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_portfolio.settings')
 
 application = get_wsgi_application()
