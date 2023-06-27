843dbf5bd79dd234a7f2132fbb4e049972f7caa0
Leonardo <leonardorm7@hotmail.com>
2023-06-26 22:14:12 -0500

    test1

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py dumpdata --exclude auth.permission --exclude contenttypes > data.json
-python manage.py loaddata data.json
+python manage.py shell -c "exec(open('projectslist.py').read())"
+# python manage.py dumpdata --exclude auth.permission --exclude contenttypes > data.json
+# python manage.py loaddata data.json
 
