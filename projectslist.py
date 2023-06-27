b19703affcc226ce396b7d4edf0dcf51439e2081
Leonardo <leonardorm7@hotmail.com>
2023-06-26 22:37:16 -0500

    adding proyects

--- projectslist.py
+++ projectslist.py
@@ -3,15 +3,21 @@ from portfolio.models import Project
 project1 = Project(
     title='Portfolio', 
     description='Web page for project showcase with image upload and tool selection for portfolio display.', 
-    url='https://github.com/Engleonardorm7/Portfolio')
+    url='https://github.com/Engleonardorm7/Portfolio',
+    tools_box='django',
+    tools_box2='python',
+    tools_box3='html'),
 project1.image.save('media/portfolio/images/Portfolio.png', open('media/portfolio/images/Portfolio.png', 'rb'), 
 save=True)
-tools_box='django'
-tools_box2='python'
-tools_box3='html'
-
-
 project1.save()
 
-project2 = Project(title='Weight', description='Aplicación para trackear peso')
-project2.save()
+project2 = Project(
+    title='weight tracking', 
+    description='This is a weight tracking project using django', 
+    url='https://github.com/Engleonardorm7/Django/tree/main/Gym_progression',
+    tools_box='django',
+    tools_box2='python',
+    tools_box3='html'),
+project1.image.save('media/portfolio/images/gym_progression.png', open('media/portfolio/images/gym_progression.png', 'rb'), 
+save=True)
+project1.save()
