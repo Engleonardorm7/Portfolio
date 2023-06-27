4b3629674d484fb7fadb91aa4286918cdc3850f7
Leonardo <leonardorm7@hotmail.com>
2023-06-26 21:23:04 -0500

    changing email address

--- build.sh
+++ build.sh
@@ -8,3 +8,11 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py dumpdata --exclude auth.permission --exclude contenttypes > data.json
 python manage.py loaddata data.json
+
+python manage.py shell
+from django.contrib.auth.models import User
+
+# Crea un nuevo usuario
+user = User.objects.create_user(username='Leonardo', password='1234')
+user.is_superuser = True
+user.save()
