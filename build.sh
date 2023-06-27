67a1e9843e664540d34f6ebe02dfbfc8831c3518
Leonardo <leonardorm7@hotmail.com>
2023-06-26 22:59:44 -0500

    adding proyects

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
 
-python manage.py shell -c "exec(open('projectslist.py').read())"
+# python manage.py shell -c "exec(open('projectslist.py').read())"
+
 # python manage.py dumpdata --exclude auth.permission --exclude contenttypes > data.json
 # python manage.py loaddata data.json
 
