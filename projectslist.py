c3ae3fb486a67488805e97571d090bd2278bbb36
Leonardo <leonardorm7@hotmail.com>
2023-06-27 15:50:00 -0500

    trying with urls to show the images

--- projectslist.py
+++ projectslist.py
@@ -21,16 +21,12 @@ project1.save()
 project2 = Project(
     title='weight tracking', 
     description='This is a weight tracking project using django', 
-    url='https://github.com/Engleonardorm7/Django/tree/main/Gym_progression',
+    image='https://github.com/Engleonardorm7/Portfolio/blob/Deploy/media/portfolio/images/Restaurant_corta_API_hHA3ZPs.png?raw=true',
     tools_box='django',
     tools_box2='python',
     tools_box3='html',
     
 )
-image_path = os.path.join(settings.MEDIA_ROOT, 'portfolio/images/shutterstock_605291450.jpg')
-
-with open(image_path, 'rb') as file:
-    project2.image.save('shutterstock_605291450.jpg', File(file), save=True)
 
 
 project2.save()
