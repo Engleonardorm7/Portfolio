1bff38de6062b3c5299ff1bef8429af9515b7e53
Leonardo <leonardorm7@hotmail.com>
2024-09-25 01:07:45 +0200

    urls edited

--- Django_portfolio/urls.py
+++ Django_portfolio/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'), 
+    path('api/', include('portfolio.urls')),
     path('blog/',include('blog.urls')),
 ]
 
